fe6aef40335b363c04a33e4ad926e695bac16aba
nr.izygin <nr.izygin@tensor.ru>
2019-05-14 19:13:40 +0300

    sibling mounting

--- packages/inferno/src/DOM/mounting.ts
+++ packages/inferno/src/DOM/mounting.ts
@@ -179,6 +179,9 @@ export function mountArrayChildren(children, dom: Element | null, context: Objec
     if (child.flags & VNodeFlags.InUse) {
       children[i] = child = directClone(child);
     }
+    if (child.controlClass) {
+      child.sibling = children[i+1];
+    }
     mount(child, dom, context, isSVG, nextNode, lifecycle, false, environment, parentControlNode, parentVNode);
   }
 }
@@ -647,6 +650,9 @@ export function mountWasabyControl(vNode: any, parentDOM: Element | null, isSVG:
               delete environment.asyncRenderIds[VirtualNode.instance.id];
               if (VirtualNode.compound || (VirtualNode.instance.markup && VirtualNode.instance.markup.type !== 'invisible-node')) {
                  VirtualNode = setWasabyControlNodeHooks(VirtualNode.instance, VirtualNode, parentVNode, isRootStart, parentDOM, lifecycle, environment);
+                 if (VirtualNode.sibling) {
+                   nextNode = VirtualNode.sibling;
+                 }
                  mount(VirtualNode.instance.markup, parentDOM, {}, isSVG, nextNode, lifecycle, isRootStart, environment, VirtualNode.instance, VirtualNode);
               }
               if (Object.keys(environment.asyncRenderIds).length === 0) {
