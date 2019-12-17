13da9df0a7547a43e3a01b13d0c912525f16b57c
fa.kolbeshin <fa.kolbeshin@tensor.ru>
2019-12-17 12:37:56 +0300

    preparing inferno from sources for building into SBISPlatformSDK

--- wasaby-compat.js
+++ wasaby-compat.js
@@ -1,38 +1,67 @@
-let fs = require('fs'),
+const
+    fs = require('fs-extra'),
+    path = require('path'),
     infernoFilePath = '/packages/inferno/dist/index.cjs',
     hydrateFilePath = '/packages/inferno-hydrate/dist/index.cjs',
-    hydratePath = '/Inferno/third-party/hydrate',
     infernoVarName = 'infernoSource',
-    srcInfernoPath = '/Inferno/third-party/index';
+    infernoIndexAmd = 'Inferno/third-party/index',
+    infernoHydrateAmd = 'Inferno/third-party/hydrate';
 
+function unixifyPath(filePath) {
+  return filePath.replace(/\\/g, '/');
+}
 function replaceRequire(data) {
     return data.replace('require("inferno")', infernoVarName).replace('require(\'inferno\')', infernoVarName);
 }
- function infernoDefineModuleCreate(what, data) {
-     return `define('Inferno/${what}', ['View/Executor/Expressions', 'Core/helpers/String/unEscapeASCII','Core/detection','Core/IoC'], function (Expressions, unEscapeASCII, detection, IoC) {var exports = {}, RawMarkupNode = Expressions.RawMarkupNode; ${data} return exports;});`;
+ function infernoDefineModuleCreate(what, data, releaseMode) {
+   const result = [];
+   result.push(`define('${what}', ['View/Executor/Expressions', 'Core/helpers/String/unEscapeASCII','Env/Env', 'UI/Utils'], function (Expressions, unEscapeASCII, Env, uiUtils) {var exports = {}, RawMarkupNode = Expressions.RawMarkupNode, Logger = uiUtils.Logger; ${data} return exports;});`);
+   if (releaseMode) {
+     result.push(`define('${what}.min',['${what}'],function(infernoModule){return infernoModule;});`);
+   }
+   return result.join('\n');
  }
- function hydrateDefineModuleCreate(what, data) {
-    return `define('Inferno/${what}', ['View/Executor/Expressions', 'Core/helpers/String/unEscapeASCII','Core/detection','Core/IoC', 'Inferno/third-party/index.${what === 'third-party/hydrate.dev' ? 'dev' : 'min'}'], function (Expressions, unEscapeASCII, detection, IoC, ${infernoVarName}) {var exports = {}, RawMarkupNode = Expressions.RawMarkupNode; ${replaceRequire(data)} return exports;});`;
+ function hydrateDefineModuleCreate(what, data, releaseMode) {
+    const result = [];
+    result.push(`define('${what}', ['View/Executor/Expressions', 'Core/helpers/String/unEscapeASCII','Env/Env', 'Inferno/third-party/index', 'UI/Utils'], function (Expressions, unEscapeASCII, Env, ${infernoVarName}, uiUtils) {var exports = {}, RawMarkupNode = Expressions.RawMarkupNode, Logger = uiUtils.Logger; ${replaceRequire(data)} return exports;});`);
+    if (releaseMode) {
+      result.push(`define('${what}.min',['${what}'],function(infernoModule){return infernoModule;});`);
+    }
+    return result.join('\n');
 }
     
- function copyData(savPath, srcPath, what) {
-      fs.readFile(srcPath, 'utf8', function (err, data) {
-              if (err) throw err;
-              let src = '';
-              if (what.indexOf('hydrate') !== -1) {
-                src = hydrateDefineModuleCreate(what, data);
-              } else {
-                src = infernoDefineModuleCreate(what, data);
-              }
-              //Do your processing, MD5, send a satellite to the moon, etc.
-              fs.writeFile (savPath, src, function(error) {
-                  if (error) throw error;
-                  console.log( savPath + ' complete');
-              });
-          });
+ function copyData(pathToSave, sourcePath, amdName) {
+  return function(savPath, srcPath, what) {
+    const srcContent = fs.readFileSync(srcPath, 'utf8');
+    let src = '';
+    if (what.indexOf('hydrate') !== -1) {
+      src = hydrateDefineModuleCreate(what, srcContent, sourcePath.endsWith('min.js'));
+    } else {
+      src = infernoDefineModuleCreate(what, srcContent, sourcePath.endsWith('min.js'));
+    }
+    //Do your processing, MD5, send a satellite to the moon, etc.
+    fs.outputFileSync(savPath, src);
+    console.log( savPath + ' complete');
+  }(unixifyPath(pathToSave), unixifyPath(sourcePath), amdName);
 }
 
-copyData(__dirname + srcInfernoPath + '.dev.js', __dirname + infernoFilePath + '.js', 'Inferno/third-party/index.dev');
-copyData(__dirname + srcInfernoPath + '.min.js', __dirname + infernoFilePath + '.min.js', 'Inferno/third-party/index.min');
-copyData(__dirname + hydratePath + '.dev.js', __dirname + hydrateFilePath + '.js', 'Inferno/third-party/hydrate.dev');
-copyData(__dirname + hydratePath + '.min.js', __dirname + hydrateFilePath + '.min.js', 'Inferno/third-party/hydrate.min');
+copyData(
+  path.join(__dirname, `${infernoIndexAmd}.js`),
+  path.join(__dirname, `${infernoFilePath}.js`),
+  infernoIndexAmd
+);
+copyData(
+  path.join(__dirname, `${infernoIndexAmd}.min.js`),
+  path.join(__dirname, `${infernoFilePath}.min.js`),
+  infernoIndexAmd
+);
+copyData(
+  path.join(__dirname, `${infernoHydrateAmd}.js`),
+  path.join(__dirname, `${hydrateFilePath}.js`),
+  infernoHydrateAmd
+);
+copyData(
+  path.join(__dirname, `${infernoHydrateAmd}.min.js`),
+  path.join(__dirname, `${hydrateFilePath}.min.js`),
+  infernoHydrateAmd
+);
