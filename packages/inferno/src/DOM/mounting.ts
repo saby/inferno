618bcbf987f9ad930aec9619237a1ce856aeb14f
Служебный пользователь Инц №52255 <unix-prog_server@unix.tensor.ru@sbis-dev-mon.unix.tensor.ru>
2023-06-08 18:01:12 +0300

    new change

---
+++ packages/inferno/src/DOM/mounting.ts
@@ -0,0 +1,246 @@
+import { isFunction, isNull, isNullOrUndef, isString, isStringOrNumber, throwError, isArray } from 'inferno-shared';
+import { ChildFlags, VNodeFlags } from 'inferno-vnode-flags';
+import { createVoidVNode, directClone } from '../core/implementation';
+import { VNode } from '../core/types';
+import { documentCreateElement, EMPTY_OBJ, findDOMfromVNode, insertOrAppend } from './utils/common';
+import { mountProps } from './props';
+import { createClassComponentInstance, handleComponentInput } from './utils/componentutil';
+import { validateKeys } from '../core/validate';
+import { mountRef } from '../core/refs';
+
+export function mount(vNode: VNode, parentDOM: Element | null, context: Object, isSVG: boolean, nextNode: Element | null, lifecycle: Function[], isRootStart?: boolean): void {
+  const flags = (vNode.flags |= VNodeFlags.InUse);
+
+  if (flags & VNodeFlags.Element) {
+    mountElement(vNode, parentDOM, context, isSVG, nextNode, lifecycle, isRootStart);
+  } else if (flags & VNodeFlags.ComponentClass) {
+    mountClassComponent(vNode, parentDOM, context, isSVG, nextNode, lifecycle);
+  } else if (flags & VNodeFlags.ComponentFunction) {
+    mountFunctionalComponent(vNode, parentDOM, context, isSVG, nextNode, lifecycle);
+  } else if (flags & VNodeFlags.Void || flags & VNodeFlags.Text) {
+    mountText(vNode, parentDOM, nextNode);
+  } else if (flags & VNodeFlags.Fragment) {
+    mountFragment(vNode, parentDOM, context, isSVG, nextNode, lifecycle);
+  } else if (flags & VNodeFlags.Portal) {
+    mountPortal(vNode, context, parentDOM, nextNode, lifecycle);
+    // @ts-ignore
+  } else if (vNode.moduleName === 'UI/_executor/_Expressions/RawMarkupNode') {
+    return mountHTML(vNode, parentDOM, nextNode);
+  } else if (process.env.NODE_ENV !== 'production') {
+    // Development validation, in production we don't need to throw because it crashes anyway
+    if (typeof vNode === 'object') {
+      throwError(
+        `mount() received an object that's not a valid VNode, you should stringify it first, fix createVNode flags or call normalizeChildren. Object: "${JSON.stringify(
+          vNode
+        )}".`
+      );
+    } else {
+      throwError(`mount() expects a valid VNode, instead it received an object with the type "${typeof vNode}".`);
+    }
+  }
+}
+
+let domParser;
+
+function getDOMParser() {
+    if (!domParser) {
+        domParser = new DOMParser();
+    }
+    return domParser;
+}
+function parseHTML(markup) {
+    const result = getDOMParser().parseFromString(markup, 'text/html');
+    return result.body.firstChild;
+}
+
+export function mountHTML(vNode: VNode, parentDom: Element | null, nextNode?): any {
+  // @ts-ignore
+  const dom = (vNode.dom = parseHTML(vNode.markup));
+  if (!isNull(parentDom)) {
+    insertOrAppend(parentDom, dom, nextNode);
+  }
+  return dom;
+
+}
+
+// Safe patch Inferno for portal popup.
+// Wasaby Synchronizer can work only with array children.
+// Inferno and Wasaby Synchronizer will be removed soon, so let's choose simplier way.
+function fixPortalChildrenForWasaby(vNode): VNode {
+  if (isArray(vNode.children)) {
+    return vNode.children[0];
+  }
+  return vNode.children;
+}
+
+function mountPortal(vNode, context, parentDOM: Element | null, nextNode: Element | null, lifecycle: Function[]) {
+  mount(fixPortalChildrenForWasaby(vNode), vNode.ref, context, false, null, lifecycle);
+
+  const placeHolderVNode = createVoidVNode();
+
+  mountText(placeHolderVNode, parentDOM, nextNode);
+
+  vNode.dom = placeHolderVNode.dom;
+}
+
+function mountFragment(vNode, parentDOM, context, isSVG, nextNode, lifecycle: Function[]): void {
+  const children = vNode.children;
+
+  if (vNode.childFlags === ChildFlags.HasVNodeChildren) {
+    mount(children as VNode, parentDOM, nextNode, isSVG, nextNode, lifecycle);
+  } else {
+    mountArrayChildren(children, parentDOM, context, isSVG, nextNode, lifecycle);
+  }
+}
+
+export function mountText(vNode: VNode, parentDOM: Element | null, nextNode: Element | null): void {
+  const dom = (vNode.dom = document.createTextNode(vNode.children as string) as any);
+
+  if (!isNull(parentDOM)) {
+    insertOrAppend(parentDOM, dom, nextNode);
+  }
+}
+
+export function mountTextContent(dom: Element, children: string): void {
+  dom.textContent = children as string;
+}
+
+export function mountElement(vNode: VNode, parentDOM: Element | null, context: Object, isSVG: boolean, nextNode: Element | null, lifecycle: Function[], isRootStart?: boolean): void {
+  const flags = vNode.flags;
+  const props = vNode.props;
+  const className = vNode.className;
+  const ref = vNode.ref;
+  let children = vNode.children;
+  const childFlags = vNode.childFlags;
+  isSVG = isSVG || (flags & VNodeFlags.SvgElement) > 0;
+  const dom = isRootStart ? parentDOM : documentCreateElement(vNode.type, isSVG);
+
+  vNode.dom = dom;
+
+  if (!isNullOrUndef(className) && className !== '') {
+    if (dom) {
+      if (isSVG) {
+        dom.setAttribute('class', className);
+      } else {
+        dom.className = className;
+      }
+    }
+  }
+
+  if (process.env.NODE_ENV !== 'production') {
+    validateKeys(vNode);
+  }
+
+  if (!isNull(props)) {
+    if (vNode.type === 'link') {
+      if (dom) {
+        // @ts-ignore
+        if (props.href !== (dom.attributes.href && dom.attributes.href.value)) {
+          mountProps(vNode, flags, props, dom, isSVG);
+        }
+      }
+    } else {
+      mountProps(vNode, flags, props, dom, isSVG);
+    }
+  }
+
+  if (!isRootStart && !isNull(parentDOM)) {
+    insertOrAppend(parentDOM, dom, nextNode);
+  }
+
+  if (childFlags === ChildFlags.HasTextChildren) {
+    if (dom) {
+      mountTextContent(dom, children as string);
+    }
+  } else if (childFlags !== ChildFlags.HasInvalidChildren) {
+    const childrenIsSVG = isSVG && vNode.type !== 'foreignObject';
+
+    if (childFlags === ChildFlags.HasVNodeChildren) {
+      if ((children as VNode).flags & VNodeFlags.InUse) {
+        vNode.children = children = directClone(children as VNode);
+      }
+      mount(children as VNode, dom, context, childrenIsSVG, null, lifecycle);
+    } else if (childFlags === ChildFlags.HasKeyedChildren || childFlags === ChildFlags.HasNonKeyedChildren) {
+      if (dom) {
+        mountArrayChildren(children, dom, context, childrenIsSVG, null, lifecycle);
+      }
+    }
+  }
+
+  if (process.env.NODE_ENV !== 'production') {
+    if (isString(ref)) {
+      throwError('string "refs" are not supported in Inferno 1.0. Use callback ref or Inferno.createRef() API instead.');
+    }
+  }
+  mountRef(ref, dom, lifecycle);
+}
+
+export function mountArrayChildren(children, dom: Element | null, context: Object, isSVG: boolean, nextNode: Element | null, lifecycle: Function[]): void {
+  for (let i = 0, len = children.length; i < len; ++i) {
+    let child = children[i];
+
+    if (child.flags & VNodeFlags.InUse) {
+      children[i] = child = directClone(child);
+    }
+    mount(child, dom, context, isSVG, nextNode, lifecycle);
+  }
+}
+
+export function mountClassComponent(vNode: VNode, parentDOM: Element | null, context: Object, isSVG: boolean, nextNode: Element | null, lifecycle: Function[]) {
+  const instance = createClassComponentInstance(vNode, vNode.type, vNode.props || EMPTY_OBJ, context, isSVG, lifecycle);
+  mount(instance.$LI, parentDOM, instance.$CX, isSVG, nextNode, lifecycle);
+  mountClassComponentCallbacks(vNode.ref, instance, lifecycle);
+  instance.$UPD = false;
+}
+
+export function mountFunctionalComponent(vNode: VNode, parentDOM: Element | null, context: Object, isSVG: boolean, nextNode: Element | null, lifecycle): void {
+  const type = vNode.type;
+  const props = vNode.props || EMPTY_OBJ;
+  const ref = vNode.ref;
+
+  const input = handleComponentInput(vNode.flags & VNodeFlags.ForwardRef ? type(props, ref, context) : type(props, context));
+  vNode.children = input;
+  mount(input, parentDOM, context, isSVG, nextNode, lifecycle);
+  mountFunctionalComponentCallbacks(props, ref, vNode, lifecycle);
+}
+
+function createClassMountCallback(instance) {
+  return () => {
+    instance.$UPD = true;
+    instance.componentDidMount();
+    instance.$UPD = false;
+  };
+}
+
+export function mountClassComponentCallbacks(ref, instance, lifecycle: Function[]) {
+  mountRef(ref, instance, lifecycle);
+
+  if (process.env.NODE_ENV !== 'production') {
+    if (isStringOrNumber(ref)) {
+      throwError('string "refs" are not supported in Inferno 1.0. Use callback ref or Inferno.createRef() API instead.');
+    } else if (!isNullOrUndef(ref) && typeof ref === 'object' && ref.current === void 0) {
+      throwError('functional component lifecycle events are not supported on ES2015 class components.');
+    }
+  }
+
+  if (isFunction(instance.componentDidMount)) {
+    lifecycle.push(createClassMountCallback(instance));
+  }
+}
+
+function createOnMountCallback(ref, vNode, props) {
+  return () => {
+    ref.onComponentDidMount(findDOMfromVNode(vNode, true), props);
+  };
+}
+
+export function mountFunctionalComponentCallbacks(props, ref, vNode: VNode, lifecycle: Function[]) {
+  if (!isNullOrUndef(ref)) {
+    if (isFunction(ref.onComponentWillMount)) {
+      ref.onComponentWillMount(props);
+    }
+    if (isFunction(ref.onComponentDidMount)) {
+      lifecycle.push(createOnMountCallback(ref, vNode, props));
+    }
+  }
+}
---
+++ packages/inferno/src/DOM/props.ts
@@ -0,0 +1,204 @@
+import { namespaces } from './constants';
+import { isFunction, isNull, isNullOrUndef, isString, throwError } from 'inferno-shared';
+import { delegatedEvents, handleEvent } from './events/delegation';
+import { ChildFlags, VNodeFlags } from 'inferno-vnode-flags';
+import { isSameInnerHTML } from './utils/innerhtml';
+import { addFormElementEventHandlers, isControlledFormElement, processElement } from './wrappers/processElement';
+import { unmount, unmountAllChildren } from './unmounting';
+import { LinkedEvent, VNode } from '../core/types';
+
+function createLinkEvent(linkEvent, nextValue) {
+  return function(e) {
+    linkEvent(nextValue.data, e);
+  };
+}
+
+export function patchEvent(name: string, nextValue, dom) {
+  const nameLowerCase = name.toLowerCase();
+
+  if (!isFunction(nextValue) && !isNullOrUndef(nextValue)) {
+    const linkEvent = nextValue.event;
+
+    if (isFunction(linkEvent)) {
+      dom[nameLowerCase] = createLinkEvent(linkEvent, nextValue);
+    } else {
+      // Development warning
+      if (process.env.NODE_ENV !== 'production') {
+        throwError(`an event on a VNode "${name}". was not a function or a valid linkEvent.`);
+      }
+    }
+  } else {
+    const domEvent = dom[nameLowerCase];
+    // if the function is wrapped, that means it's been controlled by a wrapper
+    if (!domEvent || !domEvent.wrapped) {
+      dom[nameLowerCase] = nextValue;
+    }
+  }
+}
+
+// We are assuming here that we come from patchProp routine
+// -nextAttrValue cannot be null or undefined
+export function patchStyle(lastAttrValue, nextAttrValue, dom) {
+  if (isNullOrUndef(nextAttrValue)) {
+    dom.removeAttribute('style');
+    return;
+  }
+  const domStyle = dom.style;
+  let style;
+  let value;
+  if (isString(nextAttrValue)) {
+    // Do not overwrite unchanged cssText for optimization.
+    if (domStyle.cssText !== nextAttrValue) {
+      domStyle.cssText = nextAttrValue;
+    }
+    return;
+  }
+
+  if (!isNullOrUndef(lastAttrValue) && !isString(lastAttrValue)) {
+    for (style in nextAttrValue) {
+      // do not add a hasOwnProperty check here, it affects performance
+      value = nextAttrValue[style];
+      if (value !== lastAttrValue[style]) {
+        domStyle.setProperty(style, value);
+      }
+    }
+
+    for (style in lastAttrValue) {
+      if (isNullOrUndef(nextAttrValue[style])) {
+        domStyle.removeProperty(style);
+      }
+    }
+  } else {
+    for (style in nextAttrValue) {
+      value = nextAttrValue[style];
+      domStyle.setProperty(style, value);
+    }
+  }
+}
+
+export function patchProp(prop, lastValue, nextValue, dom: Element, isSVG: boolean, hasControlledValue: boolean, lastVNode: VNode | null) {
+  switch (prop) {
+    case 'children':
+    case 'childrenType':
+    case 'className':
+    case 'defaultValue':
+    case 'key':
+    case 'multiple':
+    case 'ref':
+      break;
+    case 'autoFocus':
+      (dom as any).autofocus = !!nextValue;
+      break;
+    case 'allowFullscreen':
+    case 'autoplay':
+    case 'capture':
+    case 'checked':
+    case 'controls':
+    case 'default':
+    case 'disabled':
+    case 'hidden':
+    case 'indeterminate':
+    case 'loop':
+    case 'muted':
+    case 'novalidate':
+    case 'open':
+    case 'readOnly':
+    case 'required':
+    case 'reversed':
+    case 'scoped':
+    case 'seamless':
+    case 'selected':
+      const booleanNextValue = !!nextValue;
+
+      // Do not overwrite unchanged property for optimization.
+      if (dom[prop] !== booleanNextValue) {
+        dom[prop] = booleanNextValue;
+      }
+      break;
+    case 'defaultChecked':
+    case 'value':
+    case 'volume':
+      if (hasControlledValue && prop === 'value') {
+        break;
+      }
+      const value = isNullOrUndef(nextValue) ? '' : nextValue;
+      if (dom[prop] !== value) {
+        dom[prop] = value;
+      }
+      break;
+    case 'style':
+      patchStyle(lastValue, nextValue, dom);
+      break;
+    case 'dangerouslySetInnerHTML':
+      const lastHtml = (lastValue && lastValue.__html) || '';
+      const nextHtml = (nextValue && nextValue.__html) || '';
+      if (lastHtml !== nextHtml) {
+        if (!isNullOrUndef(nextHtml) && !isSameInnerHTML(dom, nextHtml)) {
+          if (!isNull(lastVNode)) {
+            if (lastVNode.childFlags & ChildFlags.MultipleChildren) {
+              unmountAllChildren(lastVNode.children as VNode[]);
+            } else if (lastVNode.childFlags === ChildFlags.HasVNodeChildren) {
+              unmount(lastVNode.children);
+            }
+            lastVNode.children = null;
+            lastVNode.childFlags = ChildFlags.HasInvalidChildren;
+          }
+          dom.innerHTML = nextHtml;
+        }
+      }
+      break;
+    // Fix for added focus attributes to the node
+    // TODO: We have to add these attributes at the element properties on the fly
+    case 'ws-creates-context':
+      break;
+    case 'ws-delegates-tabfocus':
+      break;
+    default:
+      if (delegatedEvents[prop]) {
+        if (
+          !(
+            lastValue &&
+            nextValue &&
+            !isFunction(lastValue) &&
+            !isFunction(nextValue) &&
+            (lastValue as LinkedEvent<any, any>).event === (nextValue as LinkedEvent<any, any>).event &&
+            (lastValue as LinkedEvent<any, any>).data === (nextValue as LinkedEvent<any, any>).data
+          )
+        ) {
+          handleEvent(prop, nextValue, dom);
+        }
+      } else if (prop.charCodeAt(0) === 111 && prop.charCodeAt(1) === 110) {
+        patchEvent(prop, nextValue, dom);
+      } else if (isNullOrUndef(nextValue)) {
+        dom.removeAttribute(prop);
+      } else if (isSVG && namespaces[prop]) {
+        // We optimize for isSVG being false
+        // If we end up in this path we can read property again
+        dom.setAttributeNS(namespaces[prop], prop, nextValue);
+      } else {
+        // Do not overwrite unchanged attribute for optimization.
+        if (dom.getAttribute(prop) !== nextValue) {
+          dom.setAttribute(prop, nextValue);
+        }
+      }
+      break;
+  }
+}
+
+export function mountProps(vNode, flags, props, dom, isSVG) {
+  let hasControlledValue: boolean = false;
+  const isFormElement = (flags & VNodeFlags.FormElement) > 0;
+  if (isFormElement) {
+    hasControlledValue = isControlledFormElement(props);
+    if (hasControlledValue) {
+      addFormElementEventHandlers(flags, dom, props);
+    }
+  }
+  for (const prop in props) {
+    // do not add a hasOwnProperty check here, it affects performance
+    patchProp(prop, null, props[prop], dom, isSVG, hasControlledValue, null);
+  }
+  if (isFormElement) {
+    processElement(flags, vNode, dom, props, true, hasControlledValue);
+  }
+}
